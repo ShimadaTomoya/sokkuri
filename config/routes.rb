3584ca7dee49705dd115e4516d15d6da69078a4e
kaami <tomo.free@gmail.com>
2017-10-05 22:29:38 +0900

    [add] generate controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'compares/index'
+
+  get 'compares/show'
+
+  get 'compares/new'
+
+  get 'compares/edit'
+
+  get 'compares/create'
+
+  get 'compares/update'
+
+  get 'compares/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
